9ff5101ac40e5d96044564a946b00634da66d2fa
YnaSophiaDR <ynasophia.delrosario.cics@ust.edu.ph>
2025-02-14 02:05:44 +0800

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../main.dart';
 
 class LoginPage extends StatelessWidget {
@@ -10,16 +10,15 @@ class LoginPage extends StatelessWidget {
     return MaterialApp(
       title: "Login Page Module",
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFFFE0B6), // Set background color
+        scaffoldBackgroundColor: Color(0xFFFDE5CC),
       ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFFEDC690), // Background color
-          elevation: 0, // Remove shadow
+          backgroundColor: Color(0xFFEDC690),
+          elevation: 0,
           scrolledUnderElevation: 0,
           title: Row(
             children: [
-              // Square Image on the Left
               Container(
                 width: 50,
                 height: 50,
@@ -47,39 +46,144 @@ class LoginPage extends StatelessWidget {
             ],
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [LoginPageBtnFieldSection()],
-          ),
-        ),
         drawer: Drawer(
           child: ListView(
             children: [DrwerHeader(), DrwListView()],
           ),
         ),
+        body: Container(
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromRGBO(255, 225, 183, 1.0),
+                Color.fromRGBO(255, 225, 183, 1.0),
+                Colors.white,
+              ],
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("main_logo0.png", height: 150),
+                  SizedBox(height: 20),
+                  Text(
+                    "Welcome, User!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF462521),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  _buildTextField("Username", false),
+                  SizedBox(height: 10),
+                  _buildTextField("Password", true),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(value: false, onChanged: (value) {}),
+                          Text("Remember me"),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("Forgot password?", style: TextStyle(color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFE04F5F),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    onPressed: () {},
+                    child: Text("Log in", style: TextStyle(fontSize: 18, color: Colors.white)),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text("or Sign in with"),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                          side: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        onPressed: () {},
+                        icon: Icon(FontAwesomeIcons.facebook, color: Colors.blue),
+                        label: Text("Facebook", style: TextStyle(color: Colors.black)),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                          side: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        onPressed: () {},
+                        icon: Icon(FontAwesomeIcons.google, color: Colors.red),
+                        label: Text("Google", style: TextStyle(color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("Sign Up", style: TextStyle(color: Colors.red)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
-}
 
-class LoginPageBtnFieldSection extends StatelessWidget {
-  const LoginPageBtnFieldSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(30),
-      child: Row(
-        spacing: 10,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Expanded(
-              child: ElevatedButton.icon(
-                  onPressed: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyApp())),
-                  icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.black),
-                  label: Text("Back Home")))
-        ],
+  Widget _buildTextField(String hint, bool isPassword) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: TextFormField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hint,
+          suffixIcon: isPassword
+              ? Icon(Icons.visibility_off, color: Colors.grey)
+              : null,
+        ),
       ),
     );
   }
