27aba70fefbf56a8da40509a4962e123dbfb28e0
JuliaMelle <54196660+JuliaMelle@users.noreply.github.com>
2025-04-07 09:05:31 +0800

    Update cart_page.dart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
-
   @override
   State<CartPage> createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
+  int itemCount = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,6 +52,108 @@ class _CartPageState extends State<CartPage> {
                   ),
                   softWrap: true,
                 ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/front_donut/fdonut3.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(
+                    "Strawberry Sprinkle",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF462521),
+                    ),
+                  ),
+                  Text(
+                    "₱50.00",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF462521),
+                    ),
+                  ),
+                  ]),
+                SizedBox(width: 20),
+              Row(
+                    children: [
+                      Container (
+                        decoration: BoxDecoration(
+                         border: Border.all(
+                          color: Color(0xFFFF7171), 
+                          width: 1,
+                    ),
+                      borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SizedBox(
+                        width: 30,
+                        child: IconButton(
+                          icon: Icon(Icons.remove, color:Color(0xFFFF7171), size: 12),
+                          onPressed: () {
+                            setState(() {
+                              if (itemCount > 1) itemCount--; 
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                     SizedBox(width: 10),
+                      Text(
+                        "$itemCount", 
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF462521),
+                        ),
+                      ),
+                    SizedBox(width: 10),
+                    Container(
+                        decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                        colors: [Color(0xFFFF7171), Color(0xFFDC345E)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: SizedBox(
+                      width: 30,
+                        child:IconButton(
+                          icon: Icon(Icons.add, color: Color(0xFF462521),size: 12),
+                          onPressed: () {
+                            setState(() {
+                              itemCount++;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
